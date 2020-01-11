e99e7edbbf58f50cb7aed6c6c280ce87a17e6989
Husnul Anwari <husnulhamidiah@gmail.com>
2020-01-11 12:54:31 +0700

    Add show and download video

--- src/Media.svelte
+++ src/Media.svelte
@@ -1,5 +1,8 @@
 <script>
   export let media = []
+  let video
+
+  $: (media && video) ? video.load() : null
 
   const onClickHandler = ({ target }) => {
     const src = target.getAttribute('data-source')
@@ -11,20 +14,33 @@
   .no-padding {
     padding: 0;
   }
-  .media {
+  .image-media {
     width: 250px;
+    height: auto;
+  }
+  .video-media {
+    width: 300px;
     height: auto;
   }
 </style>
 
+<!-- TODO: Add masonry style for media -->
 <div class="row">
   {#each media as medium, i}
     {#if medium && medium.src && medium.typename}
-    <div popover-bottom="Download" class="col no-padding" on:click={ onClickHandler } data-source={ medium.src }>
-      {#if medium.typename == 'GraphImage'}
-        <img src={ medium.src } alt="media image" class="media">
-      {/if}
-    </div>
+      <div class="col no-padding">
+        <div popover-bottom="Download" on:click={ onClickHandler } data-source={ medium.src }>
+          {#if medium.typename == 'GraphImage'}
+            <img src={ medium.src } alt="media" class="media image-media">
+          {:else}
+            <div class="border-3 border-primary" style="overflow: hidden;">
+              <video bind:this={ video } class="media video-media" controls>
+                <source src={ medium.src } type="video/mp4">
+              </video>
+            </div>
+          {/if}
+        </div>
+      </div>
     {/if}
   {/each}
 </div>
