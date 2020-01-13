87fdb29e9c1a75931b77dbf79bb2019c7460d8d5
Husnul Anwari <husnulhamidiah@gmail.com>
2020-01-14 00:33:36 +0700

    Move submit button to bottom

--- src/Form.svelte
+++ src/Form.svelte
@@ -15,9 +15,8 @@
       <p style="word-break: break-word;">
         Put url <span class="hero-detail"><em>https://www.instagram.com/p/B7JqW9uA1mP/ </em></span>or just the code <span class="hero-detail"><em>B7JqW9uA1mP</em></span>
       </p>
-    </div>
-    <div class="column">
       <input class="button-primary button-large" type="submit" value="Submit" on:click|preventDefault>
     </div>
+    <div class="column"></div>
   </div>
 </form>
